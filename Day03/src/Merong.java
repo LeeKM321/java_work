b8d10af6602a3523bf957c15f7041c98ac807025
kyoungmin <stephen4951@gmail.com>
2023-02-20 18:19:42 +0900

    20230220 자바 기초 과정 진행 중

--- Day03/src/Merong.java
+++
@@ -1,8 +0,0 @@
-
-public class Merong {
-
-	public static void main(String[] args) {
-		System.out.println("merong");
-	}
-	
-}
---
+++ Java_LKM/src/basic/ScannerQuiz.java
@@ -0,0 +1,21 @@
+package basic;
+
+public class ScannerQuiz {
+
+	public static void main(String[] args) {
+		
+		/*
+		 - 스캐너를 사용하여 이름과 나이를 입력받아서
+		 
+		 이름: XXX
+		 나이: XX세
+		 출생년도: XXXX년
+		 
+		 을 출력하세요. (출력 함수는 마음대로 사용해서 출력하시고
+		 출생년도는 입력사항이 아닙니다.)
+		 */
+		
+
+	}
+
+}
