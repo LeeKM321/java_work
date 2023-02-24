cff1ecde8c4009b7bd3b6a5f843a703fe6d90aed
kyoungmin <stephen4951@gmail.com>
2023-02-24 18:13:42 +0900

    20230224 배열 탐색법까지

--- Java_LKM/src/basic/loop/WhileExample2.java
+++ Java_LKM/src/basic/loop/WhileExample2.java
@@ -39,15 +39,37 @@ public class WhileExample2 {
 			//			}
 			i++;
 		}
+		
+		System.out.println();
 
 		//1~30000까지의 정수 중 258의 배수의 개수를 출력.
-
+		int j = 1;
+		int count = 0; //배수의 개수를 기억해 줄 변수.
+		
+		while(j <= 30000) {
+			if(j % 258 == 0) {
+				count++;
+			}
+			j++;
+		}
+		System.out.println("1~30000중 258의 배수의 개수: " + count + "개");
+		
 
 		
 		//1000의 약수의 개수를 구하세요.
 		//1부터 1000까지 하나씩 올려가면서 1000이랑 나눠보세요.
 		//그랬을 때 나누어 떨어지는 애가 약수입니다.
 
+		int k = 1;
+		int cnt = 0;
+		
+		while(k <= 1000) {
+			if(1000 % k == 0) {
+				cnt++;
+			}
+			k++;
+		}
+		System.out.printf("1000의 약수의 개수: %d개\n", cnt);
 
 
 
