a87bdf3900663593ed7e6a0f08b197838f888000
kyoungmin <stephen4951@gmail.com>
2023-03-10 19:19:43 +0900

    20230310 java.util 패키지 진행중

--- Java_LKM/src/oop/obj_arr/ScoreMain.java
+++ Java_LKM/src/oop/obj_arr/ScoreMain.java
@@ -1,5 +1,7 @@
 package oop.obj_arr;
 
+import java.util.Scanner;
+
 public class ScoreMain {
 
 	public static void main(String[] args) {
@@ -20,7 +22,66 @@ public class ScoreMain {
 		  주의!) 입력이 중간에 그만두어진다면, 배열의 나머지 인덱스는
 		  모두 null로 가득 차 있습니다. (null.scoreInfo() -> 에러)
 		 */
+		
+		Score[] scores = new Score[100];
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i=0; i<scores.length; i++) {
+			System.out.println("*** 학생 점수 입력 프로그램 ***");
+			System.out.println("'그만'을 입력하시면 종료됩니다.");
+			
+			System.out.print("이름: ");
+			String name = sc.next();
+			if(name.equals("그만")) {
+				System.out.println("입력을 종료합니다.");
+				break;
+			}
+			
+			System.out.print("국어: ");
+			int kor = sc.nextInt();
+			System.out.print("영어: ");
+			int eng = sc.nextInt();
+			System.out.print("수학: ");
+			int math = sc.nextInt();
+			
+			/*
+			int total = kor + eng + math;
+			double avg = total / 3.0;
+			
+			scores[i] = new Score(name, kor, eng, math, total, avg);
+			*/
+			
+			scores[i] = new Score(name, kor, eng, math);
+			
+		} //입력 반복문
+		
+		for(Score s : scores) {
+			if(s == null) break;
+			s.scoreInfo();
+			System.out.println("----------------------------");
+		}
+		
+		sc.close();
+		
 
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
